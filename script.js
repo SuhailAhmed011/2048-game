6e732312a073c8b63a5e28338ad6789d1d4746cb
Suhail Ahmed <127311098+SuhailAhmed011@users.noreply.github.com>
2023-03-21 23:37:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let board;
-let score = 0;
-let rows = 4;
-let columns = 4;
+const input_i = document.querySelector("#input_i");
+const score_d = document.querySelector(".score");
+const board_d = document.querySelector("#board");
 
-window.onload = function() {
-    setGame();
+
+
+
+//intialize the board
+
+let width = 6;
+let score = 0;
+let square = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    start();
+});
+
+function start(){
+    score = 0;
+    square = Array.from({length: width}, () =>
+         Array.from({length: width}, () => 0)
+    );
+    game();
 }
 
-function setGame(){
-    board =[
-       [0,4,0,0]
-       [8,0,2,0]
-       [0,0,0,0]
-       [0,0,16,0]
-    ]
+function game(){
+    let tiles = "";
+    for(let i = 0; i<square.length; i++){
+        tiles += `<div class = "row">`;
+        for(let j =0; j<square[0].length; j++){
+            tiles += `<div class = "col"> ${square[i][j]} </div>`;
+        }
+        tiles += `</div>`
+    }
+    board_d.innerHTML=tiles;
 }
