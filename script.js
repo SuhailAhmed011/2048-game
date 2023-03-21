7973c1bd5d023ba5fe923b472ae9947ec203346c
Suhail Ahmed <127311098+SuhailAhmed011@users.noreply.github.com>
2023-03-21 14:19:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let board;
+let score = 0;
+let rows = 4;
+let columns = 4;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame(){
+    board =[
+       [0,4,0,0]
+       [8,0,2,0]
+       [0,0,0,0]
+       [0,0,16,0]
+    ]
+}
